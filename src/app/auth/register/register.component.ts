6c84b931a3a0bb3adbe585be5be4fe4ec243958f
Andres Gomez <andresxv98@gmail.com>
2025-05-05 14:16:44 -0500

    Add register component and form validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(100)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/)
+      ]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) { return; }
+
+    const { name, email, password } = this.registerForm.value;
+
+    // Lógica para validar emailExists
+
+    // Lógica para suscribir al service
+
+
   }
 
 }
