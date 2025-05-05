cb5543bff8a46e10df5a38a5f68f844f0362dee1
Andres Gomez <andresxv98@gmail.com>
2025-05-05 11:26:40 -0500

    Add auth modules and config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // carga el módulo de auth solo cuando se navega a /auth (lazy loading).
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
